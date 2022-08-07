60f315eec9b5e865a3d35f281ac3037ee14b0a84
Mahaoui Abdennasser <nasrosoft.dev@gmail.com>
2022-08-07 20:57:06 +0100

    add yandex map

--- src/components/UsersActivity.js
+++ src/components/UsersActivity.js
@@ -1,5 +1,30 @@
-const UsersActivity = () => {
-  return <div className=" box box4">UsersActivity</div>;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Map, Marker, MarkerLayout } from 'yandex-map-react';
+
+const UsersActivity = (props) => {
+  return (
+    <div className="UsersActivity box box4">
+      <span className="info">
+        <p>Users activity</p>
+        <p className="iInfo">
+          <a href="#">ⓘ</a>
+        </p>
+      </span>
+      <hr />
+      <Map center={[37.75837409926092, -122.44377752518518]} zoom={10}>
+        <Marker lat={37.739657514352544} lon={-122.4361199316995} />
+        <div className="user">
+          <img
+            // src={require(profile.profileImage)}
+            src={require('../images/img/Profile_image.png')}
+            alt="userName"
+          />
+          <p>Malcolm Quaday</p>
+        </div>
+      </Map>
+    </div>
+  );
 };
 
 export default UsersActivity;
