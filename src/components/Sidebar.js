41eb94794b364a1f994d12e760345c39cc361678
Mahaoui Abdennasser <nasrosoft.dev@gmail.com>
2022-08-07 20:56:07 +0100

    add logout component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,7 +5,10 @@ const Sidebar = () => {
   return (
     <div className="Sidebar  box box1">
       <div className="profile">
-        <img src={require('../images/img/Profile_image.png')} />
+        <img
+          src={require('../images/img/Profile_image.png')}
+          className="imgProfile"
+        />
         <h6>Miroslav Minev</h6>
         <span>$ 52</span>
       </div>
